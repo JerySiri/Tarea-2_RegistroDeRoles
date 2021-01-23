70815938f5fe0edb1a228aa99b28ad48a9ceb32f
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-01-23 09:56:18 -0400

    Registro de Roles conectada a base de datos con los botones guardar, editar, eliminar y validacion de los datos automatica

--- BLL/RolesBLL.cs
+++ BLL/RolesBLL.cs
@@ -77,6 +77,7 @@ namespace Tarea2_RegistroDeRoles.BLL
             Contexto contexto = new Contexto();
             bool encontrado = false;
 
+
             try
             {
                 encontrado = contexto.Roles.Any(e => e.descripcion == descripcion);
